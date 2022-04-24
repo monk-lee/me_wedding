b5db02b5e7c0be1720e4f912b9b053aac479df88
MONK_LEE <monk@senacorps.com>
2022-04-24 23:24:37 +0900

    refactor: ThanksTo 화면 리팩토링

--- src/components/ThanksTo.tsx
+++ src/components/ThanksTo.tsx
@@ -1,33 +1,36 @@
 import styled from 'styled-components';
 import Fade from 'react-reveal/Fade';
 
+import FOOTER_IMAGE from '../assets/images/footer.jpg';
+
 const ThanksTo = () => {
   return (
     <Container>
-      <Fade delay={500} bottom cascase>
-        <Title>감 / 사 / 말</Title>
-      </Fade>
-      <Divider />
-      <TextContainer>
-        <Fade delay={500} bottom cascase>
-          <TText>두 사람이 서로 사랑하며 평생을 함께 할 수 있도록</TText>
-        </Fade>
-        <Fade delay={500} bottom cascase>
-          <TText>언제나 기댈 수 있는 울타리가 되어주겠습니다.</TText>
-        </Fade>
-      </TextContainer>
-      <Divider />
-      <TextContainer>
-        <Fade delay={500} bottom cascase>
-          <TText>저희 딸의 새로운 시작을 축복해 주시는</TText>
-        </Fade>
-        <Fade delay={500} bottom cascase>
-          <TText>소중한 분들께 진심으로 감사드리며</TText>
-        </Fade>
-        <Fade delay={500} bottom cascase>
-          <TText>항상 건강하시고 행복하시길 기원하겠습니다.</TText>
-        </Fade>
-      </TextContainer>
+      <WelcomeAreaTop>
+        <WeddingText style={{ fontSize: '2vh' }}>
+          <Fade delay={1000} top cascase>
+            우연이라기엔 인연 같고,
+          </Fade>
+        </WeddingText>
+        <WeddingText style={{ fontSize: '2vh' }}>
+          <Fade delay={1000} top cascase>
+            인연이라기엔 너무 찰나 같은 순간
+          </Fade>
+        </WeddingText>
+        <WeddingText style={{ fontSize: '2vh' }}>
+          <Fade delay={1000} top cascase>
+            나는 너를 만나 사랑하게 되었다.
+          </Fade>
+        </WeddingText>
+        <WeddingText style={{ fontSize: '2vh' }}>
+          <Fade delay={1000} top cascase>
+            설레는 첫 마음 그대로 평생 간직하며 살겠습니다.
+          </Fade>
+        </WeddingText>
+      </WelcomeAreaTop>
+      <footer>
+        <PText>Copyrightⓒ 2022. MONK_LEE. All rights reserved.</PText>
+      </footer>
     </Container>
   );
 };
@@ -35,37 +38,33 @@ const ThanksTo = () => {
 const Container = styled.div`
   display: flex;
   flex-direction: column;
+  justify-content: space-between;
   align-items: center;
-  padding: 30px 0;
-  background-color: #9d7e5f;
+  position: relative;
+  height: ${window.innerHeight - 50}px;
+  padding-top: 4.5vh;
+  padding-bottom: 3vh;
+  background-image: url(${FOOTER_IMAGE});
+  background-position: top;
+  background-repeat: no-repeat;
+  background-size: cover;
 `;
 
-const Text = styled.p`
+const WelcomeAreaTop = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  line-height: 3vh;
+`;
+
+const WeddingText = styled.h1`
   font-family: 'KyoboHandwriting2020A';
 `;
 
-const TText = styled(Text)`
-  font-size: 2.5vh;
-  color: #ffffff;
-`;
-
-const Title = styled(Text)`
-  font-size: 2.5vh;
-  text-align: center;
-  color: #ffffff;
-`;
-
-const TextContainer = styled.div`
-  line-height: 3.5vh;
-  text-align: center;
-`;
-
-const Divider = styled.span`
-  background-color: #ffffff;
-  width: 40%;
-  height: 1px;
-  margin: 30px 0;
-  opacity: 0.3;
+const PText = styled.p`
+  font-size: 1.5vh;
+  font-family: Pretendard;
+  color: rgba(0, 0, 0, 0.3);
 `;
 
 export default ThanksTo;
